98ec52768a3fb37129a184f39cfb0de1219b19c6
merger <fikihaldiansyah28@gmail.com>
2024-12-19 10:01:51 +0700

    delete domain cors

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const cors = require("cors");
 
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Ganti dengan origin yang benar dari aplikasi frontend Anda
+  origin: ' ', // Ganti dengan origin yang benar dari aplikasi frontend Anda
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Jika Anda menggunakan cookies atau session
